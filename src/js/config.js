3d3e97f0a982f70e817c3137597193a0d28d2c93
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-08-26 10:07:46 +0200

    added my own cloned version of cors-anywhere

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,3 @@
 // export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const proxy = '';
+export const proxy = 'https://cors-anywhere-danogo.herokuapp.com/';
 export const key = 'c39761532888c2360b45a96ac3894b04';
