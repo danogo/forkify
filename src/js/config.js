354442ae51c54ef94d5a1d1796130b874667137b
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-08-26 09:09:03 +0200

    test without CORS proxy

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
-export const proxy = 'https://cors-anywhere.herokuapp.com/';
+// export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const proxy = '';
 export const key = 'c39761532888c2360b45a96ac3894b04';
